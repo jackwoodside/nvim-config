cbae83efb8320cea75449139aa6b88fa810cd17a
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-01-13 17:43:09 +1100

    Actually fixed cmp source priority

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -62,10 +62,11 @@ cmp.setup({
 	}),
 
 	sources = cmp.config.sources({
-		{ name = "buffer", max_item_count = 10, group_index = 3 },
-		{ name = "nvim_lsp", max_item_count = 10, group_index = 2 },
-		{ name = "luasnip", max_item_count = 10, group_index = 1 },
-		{ name = "path", max_item_count = 10, group_index = 4 },
+		{ name = "nvim_lsp", max_item_count = 10 },
+		{ name = "luasnip", max_item_count = 10 },
+	}, {
+		{ name = "buffer", max_item_count = 10 },
+		{ name = "path", max_item_count = 10 },
 	}),
 
 	snippet = {
