7655edd090e968903289a42586f84e305f8b96f9
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-01-15 17:52:32 +1100

    Added lazygit binding for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,3 +7,12 @@ require("toggleterm").setup({
 		enabled = false,
 	},
 })
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _Lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _Lazygit_toggle()<CR>", { noremap = true, silent = true })
