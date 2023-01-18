aaa6761bcf87a3192bda23b90da86b9b83a66932
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-01-18 22:20:43 +1100

    Fixed minor grammar error in lualine wordcount

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -17,7 +17,11 @@ va.nvim_create_autocmd("BufWritePost", {
 	callback = function()
 		if vf.expand("%:e") == "tex" then
 			local words = vf.system("texcount " .. vf.shellescape(vf.expand("%:p")) .. " | awk 'FNR==3 {printf $NF}'")
-			va.nvim_buf_set_var(0, "words", words)
+			if words == 1 then
+				va.nvim_buf_set_var(0, "words", words .. " word")
+			else
+				va.nvim_buf_set_var(0, "words", words .. " words")
+			end
 		end
 	end,
 })
