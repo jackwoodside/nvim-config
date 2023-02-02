4adfd518ea2418c973ac1725e31656b65caca964
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-02-02 19:17:13 +1100

    Simplify cmp tab bindings, separate bindings for snippets used

--- lua/plugins/language/cmp.lua
+++ lua/plugins/language/cmp.lua
@@ -41,24 +41,8 @@ cmp.setup({
 		["<C-PageUp>"] = cmp.mapping.scroll_docs(-4),
 		["<C-PageDown>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+		["<Tab>"] = cmp.mapping(cmp.select_next_item(), { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(cmp.select_prev_item(), { "i", "s" }),
 	}),
 
 	sources = cmp.config.sources({
