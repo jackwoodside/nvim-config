d553c0ad8fb31488c0c09bbf8a9a5c20b350d95e
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-02-14 22:15:28 +1100

    Slightly modified dap config

--- lua/plugins/language/dap.lua
+++ lua/plugins/language/dap.lua
@@ -15,3 +15,16 @@ local sign = vim.fn.sign_define
 sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
 sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
 sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+
+require("dap").configurations.cpp = {
+	{
+		name = "Launch file",
+		type = "cppdbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/bin/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtEntry = true,
+	},
+}
