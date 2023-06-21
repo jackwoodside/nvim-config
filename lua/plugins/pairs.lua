62a4db52f112139147b9e58e5e5e32c9ba8e1863
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-06-21 15:56:20 +1000

    Temporarily removed latex pairs

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -1,7 +1,18 @@
-require("mini.pairs").setup({
-	mappings = {
-		-- LaTeX pairs
-		["`"] = { action = "closeopen", pair = "`'", neigh_pattern = "[^\\].", register = { cr = false } },
-		["$"] = { action = "closeopen", pair = "$$", neigh_pattern = "[^\\].", register = { cr = false } },
-	},
-})
+local mp = require("mini.pairs")
+
+mp.setup()
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "tex",
+-- 	callback = function()
+-- 		mp.map_buf({ buffer = 0 }, { mode = "insert" }, { lhs = "$" }, {
+-- 			pair_info = {
+-- 				action = "closeopen",
+-- 				pair = "$$",
+-- 				neigh_pattern = "[^\\].",
+-- 				register = { cr = false },
+-- 			},
+-- 		})
+-- 	end,
+-- })
+
+-- ["`"] = { action = "closeopen", pair = "`'", neigh_pattern = "[^\\].", register = { cr = false } },
