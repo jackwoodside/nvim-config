573982b291f154b7d5d85095c8fb670f5ce5765d
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-06-24 17:11:58 +1000

    Updated surround mappings

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,6 +1,16 @@
 require("mini.surround").setup({
+	mappings = {
+		add = "ys",
+		delete = "ds",
+		find = "",
+		find_left = "",
+		highlight = "vs",
+		replace = "cs",
+		update_n_lines = "",
+	},
 	custom_surroundings = {
 		-- LaTeX surrounds
 		["$"] = { output = { left = "$", right = "$" } },
 	},
+	silent = true,
 })
