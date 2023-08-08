6919de1562842d5ed64cdffd93a4875f68849683
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2023-08-09 09:58:09 +1000

    Updated cmp priorities

--- lua/plugins/language/cmp.lua
+++ lua/plugins/language/cmp.lua
@@ -39,11 +39,11 @@ cmp.setup({
 	}),
 
 	sources = cmp.config.sources({
-		{ name = "luasnip", max_item_count = 10 },
-		{ name = "nvim_lsp", max_item_count = 10 },
+		{ name = "luasnip", group_index = 1, max_item_count = 10 },
+		{ name = "nvim_lsp", group_index = 1, max_item_count = 10 },
 	}, {
-		{ name = "buffer", max_item_count = 10 },
-		{ name = "path", max_item_count = 10 },
+		{ name = "buffer", group_index = 2, max_item_count = 10 },
+		{ name = "path", group_index = 2, max_item_count = 10 },
 	}),
 
 	matching = {
