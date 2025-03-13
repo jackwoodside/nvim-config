244b58f177caf452466ca5c16e23853931c25ff9
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2025-03-14 10:53:42 +1100

    Conform changes

--- lua/plugins/language/conform.lua
+++ lua/plugins/language/conform.lua
@@ -11,26 +11,8 @@ conform.setup({
 		sh = { "shfmt" },
 		tex = { "latexindent" },
 	},
-})
 
--- Format on save
-va.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = "*",
-	callback = function(args)
-		va.nvim_buf_set_var(args.buf, "format?", true)
-	end,
+	format_on_save = {
+		timeout_ms = 2000,
+	},
 })
-va.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		if va.nvim_buf_get_var(args.buf, "format?") then
-			require("conform").format({ bufnr = args.buf })
-		end
-	end,
-})
-
--- Toggle formatting
-vim.keymap.set("n", "<leader>lf", function()
-	local status = va.nvim_buf_get_var(0, "format?")
-	va.nvim_buf_set_var(0, "format?", not status)
-end, { desc = "Toggle formatting", silent = true })
