d214ceea38bc9e10345c8483d0cda378a09d8026
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2026-08-11 16:36:51 +1000

    Updated mini.comment config to remove treesitter reference

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,10 +1,4 @@
 require("mini.comment").setup({
-	options = {
-		custom_commentstring = function()
-			return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-		end,
-	},
-
 	mappings = {
 		comment = "<leader>c",
 		comment_line = "<leader>cc",
